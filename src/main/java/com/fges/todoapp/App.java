c6ec2b337c4e88c98a958b6cf06232eaa33c340c
sabine003 <88795763+sabine003@users.noreply.github.com>
2024-01-27 17:41:15 +0100

    Update App.java
    first update of App.java

--- src/main/java/com/fges/todoapp/App.java
+++ src/main/java/com/fges/todoapp/App.java
@@ -11,18 +11,111 @@ import org.apache.commons.cli.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-/**
- * Hello world!
- */
+
+interface FileHandler {
+    String readFile(String fileName) throws IOException;
+
+    void writeFile(String fileName, String content) throws IOException;
+}
+
+class JsonFileHandler implements FileHandler {
+    public String readFile(String fileName) throws IOException {
+        return Files.readString(Path.of(fileName));
+    }
+
+    public void writeFile(String fileName, String content) throws IOException {
+        Files.write(Path.of(fileName), content.getBytes());
+    }
+}
+
+class CsvFileHandler implements FileHandler {
+    public String readFile(String fileName) throws IOException {
+        return Files.readString(Path.of(fileName));
+    }
+
+    public void writeFile(String fileName, String content) throws IOException {
+        Files.write(Path.of(fileName), content.getBytes());
+    }
+}
+
+interface Command {
+    int execute(String[] args);
+}
+
+class InsertCommand implements Command {
+    private final FileHandler fileHandler;
+
+    public InsertCommand(FileHandler fileHandler) {
+        this.fileHandler = fileHandler;
+    }
+
+    public int execute(String[] args) {
+        if (args.length < 3) {
+            System.err.println("Missing TODO name");
+            return 1;
+        }
+
+        String todo = args[2];
+        String fileName = args[0];
+
+        try {
+            String fileContent = fileHandler.readFile(fileName);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode actualObj = mapper.readTree(fileContent);
+
+            if (actualObj instanceof MissingNode) {
+                actualObj = JsonNodeFactory.instance.arrayNode();
+            }
+
+            if (actualObj instanceof ArrayNode arrayNode) {
+                arrayNode.add(todo);
+            }
+
+            fileHandler.writeFile(fileName, actualObj.toString());
+            System.err.println("Task inserted.");
+            return 0;
+        } catch (IOException e) {
+            System.err.println("Error reading or writing to the file: " + e.getMessage());
+            return 1;
+        }
+    }
+}
+
+class ListCommand implements Command {
+    private final FileHandler fileHandler;
+
+    public ListCommand(FileHandler fileHandler) {
+        this.fileHandler = fileHandler;
+    }
+
+    public int execute(String[] args) {
+        String fileName = args[0];
+
+        try {
+            String fileContent = fileHandler.readFile(fileName);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode actualObj = mapper.readTree(fileContent);
+
+            if (actualObj instanceof MissingNode) {
+                actualObj = JsonNodeFactory.instance.arrayNode();
+            }
+
+            if (actualObj instanceof ArrayNode arrayNode) {
+                arrayNode.forEach(node -> System.out.println("- " + node.toString()));
+            }
+
+            System.err.println("Listed tasks.");
+            return 0;
+        } catch (IOException e) {
+            System.err.println("Error reading from the file: " + e.getMessage());
+            return 1;
+        }
+    }
+}
+
 public class App {
-
     /**
      * Do not change this method
      */
@@ -30,96 +123,35 @@ public class App {
         System.exit(exec(args));
     }
 
-    public static int exec(String[] args) throws IOException {
-        Options cliOptions = new Options();
-        CommandLineParser parser = new DefaultParser();
-
-        cliOptions.addRequiredOption("s", "source", true, "File containing the todos");
-
-        CommandLine cmd;
-        try {
-            cmd = parser.parse(cliOptions, args);
-        } catch (ParseException ex) {
-            System.err.println("Fail to parse arguments: " + ex.getMessage());
+    public static int exec(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Missing command or file name");
             return 1;
         }
 
-        String fileName = cmd.getOptionValue("s");
+        String command = args[1];
+        String fileName = args[0];
 
-        List<String> positionalArgs = cmd.getArgList();
-        if (positionalArgs.isEmpty()) {
-            System.err.println("Missing Command");
+        FileHandler fileHandler;
+        if (fileName.endsWith(".json")) {
+            fileHandler = new JsonFileHandler();
+        } else if (fileName.endsWith(".csv")) {
+            fileHandler =  new CsvFileHandler();
+        } else {
+            System.err.println("Unsupported file format");
             return 1;
         }
 
-        String command = positionalArgs.get(0);
-
-        Path filePath = Paths.get(fileName);
-
-        String fileContent = "";
-
-        if (Files.exists(filePath)) {
-            fileContent = Files.readString(filePath);
-        }
-
+        Command commandExecutor;
         if (command.equals("insert")) {
-            if (positionalArgs.size() < 2) {
-                System.err.println("Missing TODO name");
-                return 1;
-            }
-            String todo = positionalArgs.get(1);
-
-            if (fileName.endsWith(".json")) {
-                // JSON
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode actualObj = mapper.readTree(fileContent);
-                if (actualObj instanceof MissingNode) {
-                    // Node was not reconised
-                    actualObj = JsonNodeFactory.instance.arrayNode();
-                }
-
-                if (actualObj instanceof ArrayNode arrayNode) {
-                    arrayNode.add(todo);
-                }
-
-                Files.writeString(filePath, actualObj.toString());
-            }
-            if (fileName.endsWith(".csv")) {
-                // CSV
-                if (!fileContent.endsWith("\n") && !fileContent.isEmpty()) {
-                    fileContent += "\n";
-                }
-                fileContent += todo;
-
-                Files.writeString(filePath, fileContent);
-            }
+            commandExecutor = new InsertCommand(fileHandler);
+        } else if (command.equals("list")) {
+            commandExecutor = new ListCommand(fileHandler);
+        } else {
+            System.err.println("Unknown command");
+            return 1;
         }
 
-
-        if (command.equals("list")) {
-            if (fileName.endsWith(".json")) {
-                // JSON
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode actualObj = mapper.readTree(fileContent);
-                if (actualObj instanceof MissingNode) {
-                    // Node was not recognised
-                    actualObj = JsonNodeFactory.instance.arrayNode();
-                }
-
-                if (actualObj instanceof ArrayNode arrayNode) {
-                    arrayNode.forEach(node -> System.out.println("- " + node.toString()));
-                }
-            }
-            if (fileName.endsWith(".csv")) {
-                // CSV
-                System.out.println(Arrays.stream(fileContent.split("\n"))
-                        .map(todo -> "- " + todo)
-                        .collect(Collectors.joining("\n"))
-                );
-            }
-        }
-
-        System.err.println("Done.");
-        return 0;
+        return commandExecutor.execute(args);
     }
 }
