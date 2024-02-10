8be04f11579fcf71cb34cecfebc0754105264833
sabine003 <88795763+sabine003@users.noreply.github.com>
2024-02-10 15:44:33 +0100

    Update FileHandler.java

--- src/main/java/com/fges/todoapp/FileHandler.java
+++ src/main/java/com/fges/todoapp/FileHandler.java
@@ -3,7 +3,6 @@ package com.fges.todoapp;
 import java.io.IOException;
 
 public interface FileHandler {
-
     void insert(String todo, boolean markAsDone) throws IOException;
-    void list() throws IOException;
+    void list(boolean onlyDone) throws IOException;
 }
