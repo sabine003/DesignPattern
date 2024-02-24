639253896a9a612fe707d73a1349b690b5fb0526
sabine003 <88795763+sabine003@users.noreply.github.com>
2024-02-24 15:26:31 +0100

    Update TodoFileExec.java

--- src/main/java/com/fges/todoapp/TodoFileExec.java
+++ src/main/java/com/fges/todoapp/TodoFileExec.java
@@ -1,19 +1,43 @@
 package com.fges.todoapp;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ListCommand implements Command {
-    private final TodoFileExec fileExec;
-    private final boolean onlyDone;
+public class TodoFileExec {
 
-    public ListCommand(TodoFileExec fileExec, boolean onlyDone) {
-        this.fileExec = fileExec;
-        this.onlyDone = onlyDone;
+    private final String fileName;
+
+    public TodoFileExec(String fileName) {
+        this.fileName = fileName;
     }
 
-    @Override
-    public int execute(List<String> args) throws IOException {
-        return fileExec.listTodos(onlyDone);
+    public int insertTodo(String todo, boolean markAsDone) {
+        try {
+            FileHandler handler = FileHandlerFactory.createHandler(fileName);
+            handler.insert(todo, markAsDone);
+            return 0;
+        } catch (IOException e) {
+            System.err.println("Error while inserting TODO: " + e.getMessage());
+            return 1;
+        }
+    }
+
+    public int listTodos(boolean onlyDone) {
+        try {
+            FileHandler handler = FileHandlerFactory.createHandler(fileName);
+            handler.list(onlyDone);
+            return 0;
+        } catch (IOException e) {
+            System.err.println("Error while listing TODOs: " + e.getMessage());
+            return 1;
+        }
+    }
+
+    public List<String> getAllTodos() throws IOException {
+        FileHandler handler = FileHandlerFactory.createHandler(fileName);
+        List<String> todos = new ArrayList<>();
+        handler.list(false); // Get all todos
+        return todos;
     }
 }
